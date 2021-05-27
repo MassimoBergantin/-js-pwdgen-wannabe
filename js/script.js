46011f6b984c2d0fe09ad026c32a26f20030c214
MassimoBergantin <maxbergax@gmail.com>
2021-05-27 18:01:23 +0200

    js-pwdgen-wannabe

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var a = prompt("Qual'è il tuo nome")
+var b = prompt("E il tuo cognome?")
+var c = prompt("il tuo colore preferito invece?")
+var d = prompt("il tuo corso per sviluppatore web preferito?")
+
+document.getElementById ("Password").innerHTML = a + b + c + d + "21"
+
+
